cea06464bc42b0028ff9ff7dd9e1e105da9456ce
Geert Eltink <9497586+geerteltink@users.noreply.github.com>
2023-05-30 14:02:05 +0200

    feat: 2023-06-01 (#421)

--- add-post.sh
+++ add-post.sh
@@ -4,6 +4,7 @@ set -e
 echo -e "Updating local repo...\n"
 git switch main
 git pull --rebase --autostash --prune
+npm install
 
 if [ -z "$1" ]; then
     echo "ERROR: Expected date (YYYY-mm-dd) as the first argument"
